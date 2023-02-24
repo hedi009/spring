b23324b17305b3854caf4632cc1293f927a0f924
hedi009 <hedibenjmeaa09@gmail.com>
2023-02-24 13:06:34 +0100

    second commit

--- src/test/java/com/example/stageex1/service/ProduitServiceTest.java
+++ src/test/java/com/example/stageex1/service/ProduitServiceTest.java
@@ -1,81 +1,125 @@
 package com.example.stageex1.service;
 
+import com.example.stageex1.controller.ProduitControlleur;
+import com.example.stageex1.entites.Categorie;
+import com.example.stageex1.entites.Produit;
+import com.example.stageex1.repositories.CategorieRepository;
+import com.example.stageex1.repositories.ProduitRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.assertj.core.api.Assertions.assertThat;
+import java.text.ParseException;
+import java.util.List;
+import java.util.Optional;
 
-//import static com.example.Stage_EX1.service.ProduitService.pr;
+import static org.junit.Assert.*;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-
-
-class ProduitServiceTest {
+ public class ProduitServiceTest {
     @Autowired
-    private ProduitService produitService;
+    private ProduitService service;
     @Autowired
-    private CategoriesService categoriesService ;
+    ProduitControlleur produitController;
+    @Autowired
+    private CategoriesService categorieService;
+    @Autowired
+    private CategorieRepository categorieRepository;
+    @Autowired
+    private ProduitRepository produitRepository;
 
 
-   /* @Test
+    @Test
     public void SaveProduitSucces(){
         Categorie categories = new Categorie();
-        Categorie savedCategorie = categoriesService.ajout(categories,);
+        categories.setNom("aa");
+        categories.setQt(120L);
+        Categorie savedCategorie = categorieService.ajout(categories);
         Produit expectedProduit = Produit.builder()
                 .nomp("name prod")
-                .QTP(1L)
-                .Disponible(true)
-                .id(1L)
+                .qtp(1L)
+                .disponible(true)
+                .categorie(savedCategorie)
                 .build();
-        Produit savedProduit = produitService.AjouterP(expectedProduit);
+        Produit savedProduit = service.ajouterP(expectedProduit, expectedProduit.getCategorie().getId());
         assertNotNull(savedProduit);
-        assertNotNull(savedProduit.getId());
-        assertNotNull(expectedProduit.getQTP() , String.valueOf(savedProduit.getQTP()));
         assertNotNull(expectedProduit.getNomp() , savedProduit.getNomp());
 
 
     }
 
-@Test
-public void UpdateProduitSucces(){
-    Categorie category = categorieRepository.findById(10L).orElse(null);
-    assertNotNull(category);
-    Produit produit = Pr.findById(14L).orElse(null);
-    assertNotNull(produit);
-    produit.setNomp("test test test");
-    produit.setDisponible(true);
-    produit.setId(category);
-    produit.setQTP(140L);
-    Produit savedProd=produitService.AjouterP(produit);
-
-    Produit upadateProduit = savedProd;
-    savedProd = produitService.update(savedProd.getId(), upadateProduit);
-
-
-    assertNotNull(upadateProduit);
-    assertNotNull(upadateProduit.getId());
-    assertNotNull(upadateProduit.getQTP() , String.valueOf(savedProd.getQTP()));
-    assertNotNull(upadateProduit.getNomp() , savedProd.getNomp());
-}
     @Test
-    public void DeleteProduitSucces(){
-        Categorie categories = new Categorie();
+    public void testUpdateProduct() {
         Produit produit = new Produit();
-        Categorie savedCategorie = categoriesService.ajout(categories);
-        Produit expectedProduit = Produit.builder()
-                .id(12L)
-                .nomp("name prod")
-                .QTP(1L)
-                .Disponible(true)
-                .id(savedCategorie)
-                .build();
-        Produit savedProd=produitService.AjouterP(expectedProduit);
+        Categorie categorie = new Categorie();
+        categorie.setNom("Test Category");
 
-        boolean isDelted= produitService.delete(Math.toIntExact(savedProd.getId()));
-        assertTrue(isDelted);
-        Optional<Produit> optionalProduit= Pr.findById(savedProd.getId());
+        Categorie savedCategorie = categorieService.ajout(categorie);
+        produit.setCategorie(savedCategorie);
+        Long categorieId = savedCategorie.getId();
+
+        Produit produitSaved = service.ajouterP(produit,categorieId);
+
+        produitSaved.setNomp("Updated Test Product");
+        produitSaved.setQtp(20L);
+        produitSaved.setDisponible(true);
+
+        service.update(produit.getId(), produitSaved, categorieId);
+
+        Optional<Produit> produitRetrieved = service.findById(produitSaved.getId());
+        assertTrue(produitRetrieved.isPresent());
+        Produit retrievedProduct = produitRetrieved.get();
+        assertEquals("Updated Test Product", retrievedProduct.getNomp());
+        assertEquals(Optional.of(20L), retrievedProduct.getQtp());
+        assertEquals(true, retrievedProduct.isDisponible());
+    }
+
+    @Test
+    public void testDeleteProduit() throws ParseException {
+        Produit produit = new Produit();
+        Categorie categorie = new Categorie();
+        categorie.setNom("Test");
+
+        Categorie savedCategorie = categorieService.ajout(categorie);
+        produit.setNomp("produit test");
+        produit.setDisponible(true);
+        produit.setCategorie(savedCategorie);
+        produit.setQtp(10L);
+        Long categorieId = savedCategorie.getId();
+        produit = service.ajouterP(produit,categorieId);
+        boolean isDeleted = service.delete(Math.toIntExact(produit.getId()));
+        assertTrue(isDeleted);
+        Optional<Produit> optionalProduit = produitRepository.findById(produit.getId());
         assertFalse(optionalProduit.isPresent());
     }
-*/
+
+    @Test
+    public void FindAllSucces() {
+        List<Produit> produit =service.findall();
+        assertThat(produit).isNotNull();
+    }
+
+    @Test
+    public void testFindProduitById() {
+        Categorie categorie = new Categorie();
+        categorie.setNom("Test Category");
+        categorie.setQt(5L);
+        Categorie savedCategorie = categorieService.ajout(categorie);
+
+        Produit produit = new Produit();
+        produit.setNomp("Test Product");
+        produit.setQtp(10L);
+        produit.setCategorie(savedCategorie);
+        Produit savedProduit = service.ajouterP(produit,savedCategorie.getId());
+
+
+        Optional<Produit> foundProduit = service.findById(savedProduit.getId());
+        assertThat(foundProduit).isNotNull();
+
+    }
+
 }
